cba15b94b4e042bf0c28eef1efa9a3a68765c6a4
Markus <markus.kloepper@gmx.net>
2022-11-03 10:16:19 +0100

    hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import "./css/Hero.css";
+import image from "./images/heroimg.png";
+
+export default function Hero() {
+  return (
+    <div className="hero-wrapper">
+      <img src={image} alt="images" className="hero-img" />
+      <div className="hero-text-section">
+        <h1 className="hero-title">Online Experiences</h1>
+        <p className="hero-txt">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import "./css/Navbar.css";
+import logo from "./images/airbnb 1.png";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="airbnb logo" className="nav-logo"/>
+    </nav>
+  );
+}
