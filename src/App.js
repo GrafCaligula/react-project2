74fb4c8ca7fb901c899a32d78b41b18b85c0651f
Markus <markus.kloepper@gmx.net>
2022-11-03 14:17:43 +0100

    weiter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import Contacts from "./components/Contacts";
 
-export default function App(){
+export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Contacts />
-      <Card />
+      <Card
+        img="zaferes.png"
+        rating={5.0}
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+      />
+      <Card
+        img="wedding.png"
+        rating={5.0}
+        reviewCount={30}
+        country="USA"
+        title="Learning wedding photography"
+        price={125}
+      />
     </div>
-  )
+  );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,24 @@
-import cardImg from "./images/zaferes.png";
 import star from "./images/Star 1.png";
 import "./css/Card.css";
-export default function Card() {
+
+export default function Card(props) {
   return (
     <div className="card">
-      <img src={cardImg} alt="card-img" className="card-img" />
+      <img
+        src={require(`./images/${props.img}`)}
+        alt="card-img"
+        className="card-img"
+      />
       <div className="card-stats">
         <img src={star} alt="star" className="starlet" />
-        <span>5.0</span>
-        <span>(6) •</span>
-        <span>USA</span>
+        <span>{props.rating}</span>
+        <span>({props.reviewCount}) • </span>
+        <span>{props.country}</span>
       </div>
-      <p className="desc">Life lessons with Katie Zaferes</p>
-      <p className="price"><b>From $136</b> / person</p>
+      <p className="desc">{props.title}</p>
+      <p className="price">
+        <b>From ${props.price}</b> / person
+      </p>
     </div>
   );
 }
